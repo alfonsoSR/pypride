ea362e2b1a87bfbfb3c8f120797b8c4386f97d53
alfonso <alfonso.sr.trabajo@gmail.com>
2024-12-21 20:08:36 +0100

    Fortran extension for dehanttideinel
    - Created a scikit-build-core based framework to incorporate Fortran
      extensions to the library
    - Created a directory in source to contain fortran source code for
      extension modules
    - Created an `external` module for wrappers of fortran code [First
      iteration]
    - Added a python wrapper for the fortran code disseminated by the IERS
      Conventions center to calculate the solid tide displacement
    - Deleted the partial python implementation of dehanttideinel from the
      corresponding displacement class

---
+++ src/pride/external/iers/dehanttideinel.pyi
@@ -0,0 +1,11 @@
+import numpy as np
+
+def dehanttideinel(
+    xsta: np.ndarray,
+    yr: int,
+    month: int,
+    day: int,
+    fhr: float,
+    xsun: np.ndarray,
+    xmon: np.ndarray,
+) -> np.ndarray: ...
