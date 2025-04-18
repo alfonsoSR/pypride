cf36a76d4df87aac8754de3136fe4914cded79dd
alfonso <alfonso.sr.trabajo@gmail.com>
2025-04-18 11:29:58 +0100

    Remove dependency on nastro

--- examples/examine_results.py
+++ examples/examine_results.py
@@ -1,7 +1,6 @@
 from pride import io
 from pathlib import Path
-from nastro import plots as ng
-import numpy as np
+from matplotlib import pyplot as plt
 
 # Code of the station to be examined and output directory
 station = "Cd"
@@ -13,23 +12,15 @@ if __name__ == "__main__":
     source = output_directory / f"GR035_{station}.del"
     _, data = io.DelFile(source).read()
 
-    # Plot setup
-    setup = ng.PlotSetup(
-        figsize=(6.0, 6.0),
-        xlabel="Hours past initial epoch",
-        ylabel="Delay [s]",
-        dir=".",
-        name="validation.png",
-        save=False,
-        show=True,
-    )
+    fig, ax = plt.subplots(figsize=(6, 6), layout="tight")
+    mjd_00 = data[0].mjd_ref + (data[0].mjd2[0] / 86400)
 
-    # Plot data
-    with ng.SingleAxis(setup) as fig:
+    for idx, scan in enumerate(data):
 
-        mjd_00 = data[0].mjd_ref + (data[0].mjd2[0] / 86400)
+        t = ((scan.mjd_ref + (scan.mjd2 / 86400)) - mjd_00) * 24.0
+        ax.plot(t, scan.delays, ".")
 
-        for idx, scan in enumerate(data):
+    ax.set_xlabel("Hours past initial epoch")
+    ax.set_ylabel("Delay [s]")
 
-            t = ((scan.mjd_ref + (scan.mjd2 / 86400)) - mjd_00) * 24.0
-            fig.add_line(t, scan.delays, ".")
+    plt.show()
